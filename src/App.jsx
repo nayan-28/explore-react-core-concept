348b351591b59b84144ce802c78ae8c36f3e0d4e
nayan-28 <nayanmalakar720@gmail.com>
2024-02-02 19:07:03 +0600

    props

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Student></Student>
-      <Student></Student>
-      <Student></Student>
+      <Device name="Laptop" price="50000"></Device>
+      <Device name="Mobile" price="1500"></Device>
       <Student></Student>
     </>
+  );
+}
+
+function Device(props) {
+  return (
+    <h2>
+      This is {props.name}. Price:{props.price}
+    </h2>
   );
 }
 
@@ -15,7 +22,7 @@ function Student() {
   const money = 20;
   const person = { name: "Nayan", age: 12 };
   return (
-    <h3>
+    <h3 className="student">
       I am {person.name} with age: {person.age},i have some money about {money}{" "}
       tk
     </h3>
